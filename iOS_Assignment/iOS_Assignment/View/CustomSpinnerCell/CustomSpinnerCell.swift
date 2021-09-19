65fd29c3c1dfb40e8c226bde6964fa94c9e91e4e
NewajShovo <leoshovo@gmail.com>
2021-09-20 02:15:11 +0600

    CustomCell added, Cell inserted at indices divisible by 5

---
+++ iOS_Assignment/iOS_Assignment/View/CustomSpinnerCell/CustomSpinnerCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomSpinnerCell.swift
+//  iOS_Assignment
+//
+//  Created by leo on 19/9/21.
+//
+
+import UIKit
+
+class CustomSpinnerCell: UICollectionViewCell {
+
+    @IBOutlet weak var activityLoader: UIActivityIndicatorView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ iOS_Assignment/iOS_Assignment/View/ImageInsertionCollectionViewCell/ImageInsertionCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ImageInsertionCollectionViewCell.swift
+//  iOS_Assignment
+//
+//  Created by leo on 19/9/21.
+//
+
+import UIKit
+
+class ImageInsertionCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
